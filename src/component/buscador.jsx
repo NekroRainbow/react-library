41ca423f4e669b451dfaa2f67fce30a8bd1fc4db
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-03-19 17:55:10 -0400

    renderizados losproductos

--- src/component/buscador.jsx
+++ src/component/buscador.jsx
@@ -1,6 +1,8 @@
 import { useContext, useId } from 'react'
 import { FilterContext } from './filtros'
 import '../estilos/buscador.css'
+import { library } from '../mooks/books.json'
+import { Libros } from './libro'
 
 export function Buscador() {
   const { filters, setFilters } = useContext(FilterContext)
@@ -13,6 +15,23 @@ export function Buscador() {
       pages: event.target.value,
     }))
   }
+
+  const handleCategory = event => {
+    setFilters(previus => ({
+      ...previus,
+      category: event.target.value,
+    }))
+  }
+
+  const filtrado = () => {
+    return library.filter(value => {
+      return (
+        value.book.pages >= filters.pages &&
+        (filters.category === 'all' || filters.category === value.book.genre)
+      )
+    })
+  }
+
   return (
     <section>
       <h1>Libreria Reactiva</h1>
@@ -31,12 +50,23 @@ export function Buscador() {
         </div>
         <div>
           <label htmlFor={category}>Categoria</label>
-          <select name='category' id={category}>
+          <select name='category' id={category} onChange={handleCategory}>
             <option value='all'>Todos</option>
-            <option value='Fantasia'>Fantasia</option>
+            <option value='Fantasía'>Fantasia</option>
             <option value='Zombies'>Zombies</option>
           </select>
         </div>
+      </div>
+      <div className='estante'>
+        <ul>
+          {filtrado().map(value => {
+            return (
+              <li key={value.book.ISBN}>
+                <Libros poster={value.book.cover} title={value.book.title} />
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </section>
   )
---
+++ src/component/libro.jsx
@@ -0,0 +1,7 @@
+export function Libros({ index, poster, title }) {
+  return (
+    <div>
+      <img src={poster} alt={title} width='300px' height='500px' />
+    </div>
+  )
+}
